df1c895837d252a472294da3084cf89d9fbeb046
vtno <tino@vtno.me>
2024-03-09 23:48:50 +0200

    remove unused dep

--- mix.exs
+++ mix.exs
@@ -26,7 +26,6 @@ defmodule Exevent.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:bandit, "~> 1.2.3"},
-      {:file_system, "~> 1.0"},
       {:mox, "~>1.1.0", only: :test}
     ]
   end
